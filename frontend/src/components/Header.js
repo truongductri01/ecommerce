e152fc7837b3dea76094fcbda526e07832b8a866
Tri Truong <truongductri01@gmail.com>
2021-10-01 20:13:15 -0500

    feat: frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>ECommerce</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/cart">
+                  <Nav.Link>Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
